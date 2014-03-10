019013fd76bb4cf9b7e3d7433fb1ac01b04276ed
Jeremy Geros <jeremy453@gmail.com>
2014-03-10 12:35:57 -0700

    separate methods complete

--- src/sensei.js
+++ src/sensei.js
@@ -1,14 +1,17 @@
-var romajiMaxLen = 5; // _.max(_.collect(_.keys(romajitable), function(item) {return item.length}))
-var kanjiMaxLen = 64; // _.max(_.collect(_.values(kanji), function(item) {return item.length}))
+var Sensei = {
+  romajiMaxLen: 5, // _.max(_.collect(_.keys(romajitable), function(item) {return item.length}))
+  kanjiMaxLen: 64 // _.max(_.collect(_.values(kanji), function(item) {return item.length}))
+};
 
 Sensei.kanji = function(romaji) {
-  var kanjiResult = "", position = 0, len = kanjiMaxLen;
+  var kanjiResult = "", position = 0;
   
   while (position < romaji.length) {
+    var len = this.kanjiMaxLen, found = false;
+
     if (romaji.length - position < len) {
       len = romaji.length - position;
     }
-    found = false;
 
     while (len > 0 && !found) {
       romajiSubstring = romaji.substring(position, position + len);
@@ -36,64 +39,55 @@ Sensei.kanji = function(romaji) {
   return kanjiResult.split('')
 }
 
-Sensei.katakana = function(romaji) {
-
-}
-
 Sensei.hiragana = function(romaji) {
-  
+  return this.search(romaji).hiragana;
 }
 
-
-    } else if (current_alphabet == "katakana" || current_alphabet == "hiragana") {
-      // hiragana or katakana
-      romaji = this.character;
-      hiragana = "";
-      katakana = "";
-      pos = 0;
-      while (pos < romaji.length) {
-        len = maxlen;
-        if (romaji.length - pos < len) {
-          len = romaji.length - pos;
-        }
-        found = false;
-        while (len > 0 && !found) {
-          if (romajitable[romaji.substring(pos, pos + len)] != null) {
-            hiragana += romajitable[romaji.substring(pos, pos + len)].hiragana;
-            katakana += romajitable[romaji.substring(pos, pos + len)].katakana;
-            pos += len;
-            found = true;
-          }
-          len--;
-        }
-        if (!found) {
-          hiragana += romaji.charAt(pos);
-          katakana += romaji.charAt(pos);
-          pos++;
-        }
-      }
-      if (current_alphabet == 'katakana') {
-        return katakana;
-      } else {
-        return hiragana;
-      }
-
-    } else {
-      // do nothing
-      return this.character;
-    }
-  }
-
-  function populate_kanji_panel(romaji_to_display) {
-    $(".kanji_panel_romaji").html(romaji_to_display);
-    $(".kanji_panel").show();
-    $(".kanji_panel").html("");
-    choices = kanji[romaji_to_display].split('');
-    html = "";
-    for (i = 0; i < choices.length; i++) {
-      html += "<div class='option'>" + choices[i] + "</div>";
-    }
-    $(".kanji_panel").append(html);
-  }
-
+Sensei.katakana = function(romaji) {
+  return this.search(romaji).katakana;
 }
+
+Sensei.search = function(romaji) {
+  var position = 0, hiragana = '', katakana = '';
+  while (position < romaji.length) {
+    var len = this.romajiMaxLen, found = false;
+
+    if (romaji.length - position < len) {
+      len = romaji.length - position;
+    }
+    
+    while (len > 0 && !found) {
+      var romajiItem = romajitable[romaji.substring(position, position + len)];
+      if (romajiItem != null) {
+        hiragana += romajiItem.hiragana
+        katakana += romajiItem.katakana
+        position += len;
+        found = true;
+      }
+      len--;
+    }
+    if (!found) {
+      hiragana += romaji.charAt(pos);
+      katakana += romaji.charAt(pos);
+      pos++;
+    }
+  }
+  if (hiragana != '' || katakana != '') {
+    return {hiragana: hiragana, katakana: katakana};
+  }
+  return romaji;
+}
+
+//   function populate_kanji_panel(romaji_to_display) {
+//     $(".kanji_panel_romaji").html(romaji_to_display);
+//     $(".kanji_panel").show();
+//     $(".kanji_panel").html("");
+//     choices = kanji[romaji_to_display].split('');
+//     html = "";
+//     for (i = 0; i < choices.length; i++) {
+//       html += "<div class='option'>" + choices[i] + "</div>";
+//     }
+//     $(".kanji_panel").append(html);
+//   }
+
+// }
