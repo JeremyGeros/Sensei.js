9ee5e5960afa4aa02b99c40983b0dfb859bebedc
Jeremy Geros <jeremy453@gmail.com>
2014-03-18 12:11:09 -0600

    adding grunt test and grunt watch cmds

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,37 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     uglify: {
-      options: {
-        mangle: true
-      },
-      my_target: {
+      dist: {
         files: {
           'dist/sensei.full.min.js': ['src/sensei/kanji.js', 'src/sensei/romaji.js', 'src/sensei.js'],
           'dist/sensei.kanji.min.js': ['src/sensei/kanji.js', 'src/sensei.js'],
           'dist/sensei.no.kanji.min.js': ['src/sensei/romaji.js', 'src/sensei.js']
         }
       }
-    }
+    },
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    watch: {
+      files: ['src/*', 'test/*'],
+      tasks: ['mochaTest'],
+    },
   });
 
+
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
 
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('test', ['uglify', 'mochaTest']);
 
 };
